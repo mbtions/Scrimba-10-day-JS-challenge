a96974aa4fa6800be3625aabd125a5fb080b8d92
[Meenakshi Bharadwaj] <[minakshi29bharadwaj@gmail.com]>
2020-06-29 10:21:15 +0530

    added day 3 challenge

---
+++ day-03.js
@@ -0,0 +1,47 @@
+/*
+
+: Challenge 3 :
+
+In this challenge, you'll:
+
+Write a function that returns the sum of alternating numbers and return them.
+
+*/
+
+// Solution code
+
+function alternatingSums(array) {
+    let evenSum = 0;
+    let oddSum = 0;
+    
+    for(let i = 0; i < array.length; i++) {
+        if(i % 2 === 0) {
+            evenSum += array[i];
+        } else {
+            oddSum += array[i];
+        }
+    }
+    
+    return [evenSum, oddSum];
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('alternatingSums()', () => {
+    it('returns alternating sums of even and odd', () => {
+        // arrange
+        const nums = [50, 60, 60, 45, 70];
+        
+        // act
+        const result = alternatingSums(nums);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toEqual([180, 105]);
+    });
+});
