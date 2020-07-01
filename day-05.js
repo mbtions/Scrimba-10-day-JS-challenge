7bc56c45d99e20228fe380c8f26327217eead04e
Meenakshi Bharadwaj <59952791+minakshi29bharadwaj@users.noreply.github.com>
2020-07-01 16:22:49 +0530

    added day 5 challenge

---
+++ day-05.js
@@ -0,0 +1,50 @@
+function caseInsensitivePalindrome(str) {
+    const caselessStr = str.toLowerCase();
+    // first way to do the same problem
+    // const reversedCaselessStr = caselessStr.split('').reverse().join(''); 
+    
+    //second way to do the same problem
+    let reversedCaselessStr = '';
+    
+    for(let i = caselessStr.length - 1; i >= 0; i--) {
+        reversedCaselessStr += caselessStr[i];
+    }
+    
+    //returning the result
+    return caselessStr === reversedCaselessStr;
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('caseInsensitivePalindrome()', () => {
+    it('returns true for a case insensitive palindrome', () => {
+        // arrange
+        const str = 'AaBaa';
+        
+        // act
+        const result = caseInsensitivePalindrome(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(true);
+    });
+    
+    it('returns false when not a case insensitive palindrome', () => {
+        // arrange
+        const str = 'abac';
+        
+        // act
+        const result = caseInsensitivePalindrome(str);
+
+        // log
+        console.log("result 2: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });    
+});
