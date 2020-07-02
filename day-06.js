703b9a01407e0a9c05887bb01b7beb00e6167978
Meenakshi Bharadwaj <59952791+minakshi29bharadwaj@users.noreply.github.com>
2020-07-02 23:47:35 +0530

    Added day 6 challenge solution

---
+++ day-06.js
@@ -0,0 +1,14 @@
+function encloseInBrackets(str) { 
+    /* First way to add brackets
+       const solution1 = "("+ str +")";
+    */
+
+    /* Second way to add brackets
+       const solution2 = '($(str})'; 
+    */
+   
+    // third way to add brackets
+    const solution3 = '('.const(str, ')');
+
+    return solution3;
+}
